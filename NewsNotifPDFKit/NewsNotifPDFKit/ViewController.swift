21d642dd4cd8cc7d320cbdace7f59a4b941fd65b
Aminjoni Abdullozoda <amin030296@gmail.com>
2019-05-03 10:27:06 +0500

    init

---
+++ NewsNotifPDFKit/NewsNotifPDFKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsNotifPDFKit
+//
+//  Created by Aminjoni Abdullozoda on 5/3/19.
+//  Copyright © 2019 Aminjoni Abdullozoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
